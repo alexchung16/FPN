2c456001714b2863f7f92f42fdf159246c99c8b7
alexchung <yonganzhong@outlook.com>
2019-12-31 13:53:58 +0800

    delete invalid module

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : __init__.py.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2019/12/28 下午3:52
+# @ Software   : PyCharm
+#-------------------------------------------------------
