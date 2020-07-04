34434c382c3c847e882009579f4b62eb217f7d8f
alexchung <yonganzhong@outlook.com>
2020-07-04 17:25:33 +0800

    rename DataProcess

---
+++ data/coco/convert_tfrecord.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : convert_tfrecord.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/7/4 下午4:58
+# @ Software   : PyCharm
+#-------------------------------------------------------
+
+import os
+
+coco_path = '/media/alex/AC6A2BDB6A2BA0D6/alex_dataset/COCO_2017'
+
+if __name__ == "__main__":
+
+   pass
